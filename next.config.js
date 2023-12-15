584b912e966f72dd592baea28a4c9f3747e41ff3
reza zeraatkar <rezak@gmail.com>
2023-12-16 01:31:29 +0330

    next.config modified

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-  modularizeImports: {
-    "@mui/icons-material/?(((\\w*)?/?)*)": {
-      transform: "@mui/icons-material/{{ matches.[1] }}/{{member}}",
-    },
-    "react-icons/((\\w*)?/?)": {
-      transform: "react-icons/{{ matches.[1] }}/{{member}}",
-    },
-  },
 };
 
 module.exports = nextConfig;
