6e6821b4bd427e44349791331992edf1fdeb9961
reza zeraatkar <rezak@gmail.com>
2023-12-17 18:05:39 +0330

    Initial blog posts page - separated groups for portfolio and blogs page created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,6 @@
-import About from "@/components/about";
-import Contact from "@/components/contact";
-import Experience from "@/components/experience";
-import Intro from "@/components/intro";
-import Projects from "@/components/projects";
-import SectionDivider from "@/components/section-divider";
-import Skills from "@/components/skills";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="flex flex-col items-center px-4">
-      <Intro />
-      <SectionDivider />
-      <About />
-      <Projects />
-      <Skills />
-      <Experience />
-      <Contact />
-    </main>
-  );
+  redirect("/portfolio");
+  return <></>;
 }
