d37b5780d36f728f6bcb6a8ddc261cd8d0d8b472
rezazeraatkar1992 <154321801+rezazeraatkar1992@users.noreply.github.com>
2023-12-22 14:04:43 +0330

    Home page portfolio section edited and some layout fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,20 +9,25 @@ export default function Home() {
       <Card>
         <Link
           href='/portfolio'
-          className='group relative flex min-h-[400px] flex-col items-center gap-10 p-4 py-10 text-center'
+          className='group relative flex min-h-[400px] flex-col items-center justify-between p-4 text-center'
         >
           <Image
-            src='https://res.cloudinary.com/db7v5ycxn/image/upload/v1702675742/my-portfolio-website/gyhsu0fs1l6kfocafwcg.jpg'
+            src='https://res.cloudinary.com/db7v5ycxn/image/upload/t_portfolio-show-image/v1703239461/my-portfolio-website/image_c7pwmz.png'
             alt='Reza Zeraatkar image'
             quality='95'
             priority={true}
-            width={500}
-            height={500}
-            className='h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl'
+            width={800}
+            height={800}
+            className='h-48 w-48 rounded-md object-cover'
           />
-          <span className='flex items-center gap-1'>
-            This is the link to my portfolio website! Click on it to see my
-            portfolio.
+          <p className='flex items-center gap-1'>
+            This is the link to my portfolio website! Here, I will share my
+            skills and experiences. We will also have the opportunity to get
+            acquainted a little more with each other. Feel free to check it out.
+            <ImNewTab className='absolute bottom-2 right-2 h-4 w-4 opacity-60 transition group-hover:-translate-y-1 group-hover:translate-x-1' />
+          </p>
+          <span className='font-bold'>
+            click to see my portfolio page
             <ImNewTab className='absolute bottom-2 right-2 h-4 w-4 opacity-60 transition group-hover:-translate-y-1 group-hover:translate-x-1' />
           </span>
         </Link>
@@ -31,21 +36,21 @@ export default function Home() {
         <Link
           href='/blog'
           target='_blank'
-          className='group relative flex min-h-[400px] flex-col items-center justify-between px-4 py-4 text-center'
+          className='group relative flex min-h-[400px] flex-col items-center justify-between p-4 text-center'
         >
           <Image
-            src='https://res.cloudinary.com/db7v5ycxn/image/upload/v1702923940/my-portfolio-website/images2_wr5b36.png'
+            src='https://res.cloudinary.com/db7v5ycxn/image/upload/t_portfolio-show-image/v1702923940/my-portfolio-website/images2_wr5b36.png'
             alt='Reza Zeraatkar blog image'
             quality='95'
             priority={true}
-            width={192}
-            height={192}
+            width={800}
+            height={800}
             className='h-48 w-48 rounded-md object-cover'
           />
           <p>
             In my blog website, I will share my programming experiences, ideas,
             challenges and some learning content which would help other
-            developers throughout thier journey as a react/nextjs developer.{" "}
+            developers throughout thier journey as a react/nextjs developer.
           </p>
           <span className='font-bold'>
             click to see my blog page
