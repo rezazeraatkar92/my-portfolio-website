23dcda00fb715deafb33289e1ca0b0726090ca96
rezazeraatkar1992 <154321801+rezazeraatkar1992@users.noreply.github.com>
2023-12-24 13:42:18 +0330

    prettier code formatter run

--- src/email/contact-form-email.tsx
+++ src/email/contact-form-email.tsx
@@ -26,10 +26,10 @@ export default function ContactFormEmail({
       <Head />
       <Preview>New message from your portfolio site</Preview>
       <Tailwind>
-        <Body className="bg-gray-100 text-black">
+        <Body className='bg-gray-100 text-black'>
           <Container>
-            <Section className="bg-white borderBlack my-10 px-10 py-4 rounded-md">
-              <Heading className="leading-tight">
+            <Section className='borderBlack my-10 rounded-md bg-white px-10 py-4'>
+              <Heading className='leading-tight'>
                 You received the following message from the contact form
               </Heading>
               <Text>{message}</Text>
